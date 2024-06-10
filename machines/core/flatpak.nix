6df9c14551fa3f75a8e75454a7306de9dac0cb32
egirlcatnip <egirlcatnip@gmail.com>
2024-06-10 23:12:29 +0200

    fix: flatpak

--- machines/core/flatpak.nix
+++ machines/core/flatpak.nix
@@ -1,6 +1,8 @@
 {...}: {
   # Enable flatpak support
-  config.services.flatpak.enable = true;
+  services.flatpak.enable = true;
+
+  # Add flathub as a remote
   services.flatpak.remotes = {
     "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
     "flathub-beta" = "https://dl.flathub.org/beta-repo/flathub-beta.flatpakrepo";

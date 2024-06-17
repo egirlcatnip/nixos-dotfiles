71128a7ecc886329537a09a03519b6f93bc65896
egirlcatnip <egirlcatnip@gmail.com>
2024-06-17 23:43:35 +0200

    fix: comment window buttons

--- configuration/home-manager/egirlcatnip/gnome.nix
+++ configuration/home-manager/egirlcatnip/gnome.nix
@@ -10,6 +10,8 @@
 
   dconf = {
     enable = true;
+
+    # Set windows buttons to MS Windows Style
     settings."org/gnome/desktop/wm/preferences".button-layout = ":minimize,maximize,close";
   };
 }

6bd023774966d73106d10bcf95e87c037843c6a4
egirlcatnip <egirlcatnip@gmail.com>
2024-06-19 19:42:27 +0200

    feat: addzshrc, sratship vscode: update settings

--- machines/core/packages.nix
+++ machines/core/packages.nix
@@ -17,5 +17,6 @@
     vscode
     armcord
     fastfetch
+    starship
   ];
 }
--- machines/core/users.nix
+++ machines/core/users.nix
@@ -13,7 +13,38 @@
   };
 
   # Enable zsh shell
-  programs.zsh.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
+
+      shellInit = " eval \"$(starship init zsh)\" ";
+
+      setOptions = [
+        "HIST_FCNTL_LOCK"
+        "APPEND_HISTORY"
+        "SHARE_HISTORY"
+        "INC_APPEND_HISTORY"
+        "HIST_IGNORE_ALL_DUPS"
+        "HIST_IGNORE_SPACE"
+        "AUTO_CD"
+      ];
+
+      histFile = "$HOME/.zsh_history";
+      histSize = "10000";
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      ohMyZsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "fzf-tab"
+          "history"
+          "rust"
+        ];
+      };
+    };
+  };
+
   # Enable git
   programs.git.enable = true;
 }

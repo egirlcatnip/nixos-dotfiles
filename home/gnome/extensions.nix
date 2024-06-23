4509984689dccc969447e2a1dfee5ee47889554b
egirlcatnip <egirlcatnip@gmail.com>
2024-06-23 14:25:13 +0200

    fix: extensions

--- home/gnome/extensions.nix
+++ home/gnome/extensions.nix
@@ -4,12 +4,12 @@
     settings."org/gnome/shell" = {
       disable-user-extensions = false;
       enabled-extensions = with pkgs.gnomeExtensions; [
-        blur-my-shell
-        dash-to-panel
-        light-style
-        noannoyance-fork
-        tiling-assistant
-        light-shell
+        blur-my-shell.extensionUuid
+        dash-to-panel.extensionUuid
+        light-style.extensionUuid
+        noannoyance-fork.extensionUuid
+        tiling-assistant.extensionUuid
+        light-shell.extensionUuid
       ];
     };
   };
--- machines/core/packages/extensions.nix
+++ machines/core/packages/extensions.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
   # Packages shared by all systems
   environment.systemPackages = with pkgs.gnomeExtensions; [
-    blur-my-shell.extensionUuid
-    dash-to-panel.extensionUuid
-    light-style.extensionUuid
-    noannoyance-fork.extensionUuid
-    tiling-assistant.extensionUuid
-    light-shell.extensionUuid
+    blur-my-shell
+    dash-to-panel
+    light-style
+    noannoyance-fork
+    tiling-assistant
+    light-shell
   ];
 }

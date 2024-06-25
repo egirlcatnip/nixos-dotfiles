9488391c1b3d248ce51c50ce4a1a72895cad7823
egirlcatnip <egirlcatnip@gmail.com>
2024-06-25 12:45:55 +0200

    feat: add modified grub theme

--- machines/core/config/theming.nix
+++ machines/core/config/theming.nix
@@ -1,17 +1,23 @@
 {
   lib,
   config,
+  dotfiles,
   ...
-}: {
+}: let
+  grub_theme = "${dotfiles}/modified/catppuccin-mocha-grub-theme";
+in {
   options = {
     theming.enable = lib.mkEnableOption "global theming toggle";
   };
 
   config = lib.mkIf config.theming.enable {
-    catppuccin.enable = true;
-    boot.plymouth.catppuccin.enable = false;
-
     catppuccin.accent = "lavender";
     catppuccin.flavor = "mocha";
+
+    catppuccin.enable = true;
+    boot.plymouth.catppuccin.enable = false;
+    boot.loader.grub.catppuccin.enable = false;
+
+    boot.loader.grub.theme = grub_theme;
   };
 }
